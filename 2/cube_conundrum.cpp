d188fb8ac93c883cf7866815dae28f7d261130f7
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-12-03 19:56:42 +0100

    Solved Day 2

--- 2/cube_conundrum.cpp
+++ 2/cube_conundrum.cpp
@@ -3,13 +3,17 @@
 #include <inttypes.h>
 #include <vector>
 #include <fstream>
+#include <array>
+
+
+#define GAME_2
 
 using namespace std;
 
 enum class Color {
-  RED,
-  GREEN,
-  BLUE
+  RED = 0,
+  GREEN = 1,
+  BLUE = 2
 };
 
 struct CubeSet {
@@ -20,8 +24,8 @@ struct CubeSet {
 typedef vector<CubeSet> Revealed;
 
 struct Game {
-  vector<Revealed> revealed;
   uint8_t id;
+  array<size_t, 3> color_pairs;
 };
 
 Game parseGame(const string game);
@@ -33,11 +37,29 @@ int main (int argc, char *argv[]) {
   ifstream file("./input.txt");
 
   string buf;
+
+  #ifdef GAME_1
+  size_t sum = 0;
   while (std::getline(file, buf)) {
     if (buf.length() == 0) continue;
     Game game = parseGame(buf);
-    std::cout << "Game: " << std::to_string(game.id) << std::endl;
+    if (game.color_pairs[(int)Color::GREEN] <= 13 && game.color_pairs[(int)Color::RED] <= 12 && game.color_pairs[(int)Color::BLUE] <= 14) {
+      sum += game.id;
+    }
   }
+  cout << sum << endl;
+  #endif
+
+  #ifdef GAME_2
+  size_t sum = 0;
+  while (std::getline(file, buf)) {
+    if (buf.length() == 0) continue;
+    Game game = parseGame(buf);
+
+    sum += game.color_pairs[(int)Color::GREEN] * game.color_pairs[(int)Color::RED] * game.color_pairs[(int)Color::BLUE];
+  }
+  cout << sum << endl;
+  #endif
   return 0;
 }
 
@@ -50,13 +72,18 @@ Game parseGame(const string game) {
 
   new_game.id = (uint8_t)std::stoi(game.substr(game.find(' '), game.find(':'))); // Here the beging value could be used
 
+  array<size_t, 3> color_sum = {};
   while (begin < end) {
     auto idx = std::find(begin, end, ';');
 
     string revealed(begin, idx);
-    new_game.revealed.emplace_back(parseRevealed(revealed));
+    Revealed parsed = parseRevealed(revealed);
+    for (CubeSet set : parsed) {
+      color_sum[(size_t)set.color] = std::max(color_sum[(size_t)set.color], set.count);
+    }
     begin = ++idx;
   }
+  new_game.color_pairs = color_sum;
 
   return new_game;
 }
