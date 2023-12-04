8d6c4e7a909e228b2b67494b387e451355cacbe9
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-12-04 21:13:23 +0100

    Added read-file method to day 3

--- 3/gear_ratios/bin/main.ml
+++ 3/gear_ratios/bin/main.ml
@@ -1,1 +1,18 @@
+let read_file filename = 
+  let lines = ref [] in
+  let chan = open_in filename in
+    try
+      while true; do
+        lines := input_line chan :: !lines
+      done; !lines
+    with End_of_file ->
+      close_in chan;
+       List.rev !lines ;;
+
+let find_adjecent lines =
+  for i = 0 to Array.length lines do 
+    lines.(i)
+  done
+
 let () = print_endline "Hello, World!"
+
