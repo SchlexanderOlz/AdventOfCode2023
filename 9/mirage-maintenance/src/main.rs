7fa3774d44cf79065c04c60ac0de8070bf2f2d40
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-12-19 17:03:28 +0100

    Solved Day 9

--- 9/mirage-maintenance/src/main.rs
+++ 9/mirage-maintenance/src/main.rs
@@ -5,22 +5,28 @@ fn main() {
     let lines = file.lines();
 
     let sequences: Vec<Vec<i32>> = lines
-        .map(|line| line.split(' ').map(|elem| elem.parse().unwrap()).collect())
+        .map(|line| {
+            line.split(' ')
+                .map(|elem| elem.parse().unwrap())
+                .rev()
+                .collect()
+        })
         .collect();
 
     let mut sum = 0;
     for sequence in sequences {
-        sum += sequence.last().unwrap() + parse_sequence(&sequence);
+        let tmp = sequence.last().unwrap() + parse_sequence_higher(&sequence);
+        sum += tmp;
     }
     println!("Sum: {}", sum);
 }
 
-fn parse_sequence(sequence: &Vec<i32>) -> i32 {
+fn parse_sequence_higher(sequence: &Vec<i32>) -> i32 {
     let diffs: Vec<i32> = sequence.windows(2).map(|vals| vals[1] - vals[0]).collect();
 
     if diffs.iter().all(|elem| *elem == 0) {
         return 0;
     }
 
-    return *diffs.last().unwrap() + parse_sequence(&diffs);
+    return *diffs.last().unwrap() + parse_sequence_higher(&diffs);
 }
